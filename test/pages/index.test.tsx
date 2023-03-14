b087caf8cbc8f7b62dcf1218177904f720721423
Ryan McTague <ryan.mctague@slope.io>
2023-03-14 11:19:22 -0400

    removed boiler plate

--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -1,14 +1,14 @@
 import Home from "../../pages/index";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import { render, screen, fireEvent } from "@testing-library/react";
-
 describe("Home Page", ()=>{
   render(<Home/>)
   
   it("contains 'Ryan McTague as an h1'", ()=>{
-    const tag = screen.getByText("Ryan McTague")
-    expect(tag).toBeInTheDocument()
-    expect(tag.childNodes.length).toBe(1)
-    expect(tag.tagName.toLowerCase()).toBe('h1')
+    const el = screen.getByTestId("name-container")
+    const h1 = el.querySelector('h1')
+    expect(el.childNodes.length).toBe(1)
+    expect(h1).toBeDefined()
+    expect(h1!.innerHTML).toBe("Ryan McTague")
   })
 })
